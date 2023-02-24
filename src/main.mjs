69686f8bd7c7571d9c192fff9e3d8cf6ed6eaef7
antheboets <anthe.boets@student.ehb.be>
2023-02-24 21:43:29 +0100

    removing old code

--- src/main.mjs
+++ src/main.mjs
@@ -1,4 +1,4 @@
-import logic from './../logic.mjs'
+import logic from './lib/logic.mjs'
 import Fuse from 'fuse.js'
 import Chart from 'chart.js/auto'
 
@@ -59,46 +59,3 @@ window.addEventListener("load",async ()=>{
         intel = document.getElementById("IntelInput").value
     })
 })
-
-
-
-//const data =[{name:"abc"},{name:"aaa"},{name:"cde"},{name:"abb"}];
-
-//const ul = document.getElementById("itemUl");
-/*
-const removeChilds = (parent) => {
-    while (parent.lastChild) {
-        parent.removeChild(parent.lastChild);
-    }
-};
-
-/*
-const fuzzySearch = (text)=>{
-    let foundData = [];
-    for(let i = 0; i < data.length;i++){
-        if(data[i].name.toLowerCase().includes(text.toLowerCase())){
-            foundData.push(data[i]);
-        }
-    }
-    return foundData;
-}
-
-const search = (e,text) =>{
-    const resultData = fuzzySearch(text);
-    removeChilds(document.getElementById("itemUl"));
-    for(let i = 0; i < resultData.length;i++){
-        let li = document.createElement("li");
-        li.appendChild(document.createTextNode(resultData[i].name));
-        li.onclick = (e) => {itemSelect(e,resultData[i])};
-        ul.appendChild(li);
-    }
-};
-*/
-/*
-const itemSelect = (e,item) =>{
-    console.log(item);
-}
-*/
-
-
-//document.getElementById("itemInput").addEventListener("input",(e)=>{search(e,document.getElementById("itemInput").value)});
